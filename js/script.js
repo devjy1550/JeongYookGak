71dec1a09e96c5b1a30b1d9c30fdd369de2849dd
devjy1550 <kjy3501@naver.com>
2022-06-26 22:43:17 +0900

    전반적인 레이아웃 완성

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 window.onload = function () {
-    let em_swiper = new Swiper('.em-swiper',{
-        loop:true,
+    let em_swiper = new Swiper('.em-swiper', {
+        loop: true,
         speed: 1000,
-        effect: "fade"
-    })
+        effect: "fade",
+        autoplay: {
+            delay: 1800,
+            disableOnInteraction: false
+        },
+        navigation: {
+            prevEl: '.ems-control-prev',
+            nextEl: '.ems-control-next'
+        },
+        pagination: {
+            el: '.em-swiper-pgnt',
+            type: 'fraction',
+        }
+    });
 }
