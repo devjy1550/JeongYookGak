278625b1769e572a60519ef4d1b159fc62be3357
KimJY <kjy3501@naver.com>
2022-10-12 13:31:27 +0900

    수정작업

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
 window.onload = function () {
-    let em_swiper = new Swiper('.em-swiper', {
-        loop: true,
-        speed: 1000,
-        effect: "fade",
-        autoplay: {
-            delay: 1800,
-            disableOnInteraction: false
-        },
-        navigation: {
-            prevEl: '.ems-control-prev',
-            nextEl: '.ems-control-next'
-        },
-        pagination: {
-            el: '.em-swiper-pgnt',
-            type: 'fraction',
-        }
-    });
-}
+  let em_swiper = new Swiper(".em-swiper", {
+    loop: true,
+    speed: 1000,
+    effect: "fade",
+    autoplay: {
+      delay: 1800,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      prevEl: ".ems-control-prev",
+      nextEl: ".ems-control-next",
+    },
+    pagination: {
+      el: ".em-swiper-pgnt",
+      type: "fraction",
+    },
+  });
+};
+
+$(document).ready(function () {
+  let modalWrap = $(".modal-wrap");
+  let modalClose = $(".modal-close");
+
+  modalClose.click(function () {
+    $(".modal-wrap").hide();
+  });
+
+  modalWrap.click(function () {
+    $(".modal-wrap").hide();
+  });
+});
